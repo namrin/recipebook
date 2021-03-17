1d65ae1b496e32748acb5ea8f299456ee720748a
NAMRIN <namrin@nuventure.in>
2021-03-17 19:13:41 +0400

    Setup user model association to recipes & devise

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,17 +1,19 @@
 class RecipesController < ApplicationController
+	before_action :authenticate_user!
 	before_action :get_recipe, only: [:show, :edit, :update, :destroy]
-	def index
-		@recipes = Recipe.all
+
+	def index 
+		@recipes = current_user.recipes.all 
 	end
 	def show
 	end
 	def new
-		@recipe = Recipe.new
+		@recipe = current_user.recipes.new
 	end
 	def create
-		@recipe = Recipe.new(recipe_params)
+		@recipe = current_user.recipes.new(recipe_params)
 		if @recipe.save
-      		redirect_to @recipe
+      		redirect_to user_recipe_path(current_user, @recipe)
     	else
     		render :new
     	end
@@ -20,18 +22,18 @@ class RecipesController < ApplicationController
 	end
 	def update
 		if @recipe.update(recipe_params)
-      		redirect_to @recipe
+      		redirect_to user_recipe_path(current_user, @recipe)
     	else
     		render :edit
     	end
 	end
 	def destroy
 		@recipe.destroy
-		redirect_to root_path
+		redirect_to user_recipes_path(current_user)
 	end
 	private
 		def get_recipe
-			@recipe = Recipe.find(params[:id])
+			@recipe = current_user.recipes.find(params[:id])
 		end
 		def recipe_params
 			params.require(:recipe).permit(:name,:author,:ingredients,:directions,:category,:reference,:image,:note)
---
+++ test/system/users_test.rb
@@ -0,0 +1,21 @@
+require "application_system_test_case"
+
+class UsersTest < ApplicationSystemTestCase
+  setup do
+    @user = users(:one)
+  end
+
+  test "visiting the index" do
+    visit users_url
+    assert_selector "h1", text: "Users"
+  end
+
+  test "destroying a User" do
+    visit users_url
+    page.accept_confirm do
+      click_on "Destroy", match: :first
+    end
+
+    assert_text "User was successfully destroyed"
+  end
+end
